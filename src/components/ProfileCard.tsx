6cb71bb1460f76e60b34294e4fdb7cdeec13f2a3
HBerglund <herman.e.berglund@gmail.com>
2021-11-23 13:26:06 +0100

    add component for SocialButton

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,40 +1,30 @@
-import {
-  createStyles,
-  IconButton,
-  makeStyles,
-  Typography,
-} from "@material-ui/core";
-import profileImg from "../assets/profile.png";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import { useState } from "react";
+import { createStyles, makeStyles, Typography } from '@material-ui/core';
+import profileImg from '../assets/profile.png';
+import { useState } from 'react';
+import SocialButton from './SocialButton';
 
 function ProfileCard() {
   const useStyles = makeStyles(() =>
     createStyles({
       root: {
-        marginTop: "-15rem ",
+        marginTop: '-15rem ',
       },
       wrapper: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#44475a",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#44475a',
       },
       profileImg: {
-        width: "15rem",
-        marginBottom: "1rem",
+        width: '15rem',
+        marginBottom: '1rem',
       },
     })
   );
   const classes = useStyles();
 
-  const githubUrl: string = "https://github.com/hberglund";
-  const linkedInUrl: string =
-    "https://linkedin.com/in/herman-berglund-8b8939100";
-
-  const [isHovered, setIsHovered] = useState("");
+  const [isHovered, setIsHovered] = useState('');
 
   const handleHover = (name: string) => {
     setIsHovered(name);
@@ -43,40 +33,24 @@ function ProfileCard() {
   return (
     <div className={classes.root}>
       <div className={classes.wrapper}>
-        <img className={classes.profileImg} src={profileImg} alt="" />
-        <Typography component="h5" variant="h5">
+        <img className={classes.profileImg} src={profileImg} alt='' />
+        <Typography component='h5' variant='h5'>
           Hi, I'm Herman!
         </Typography>
-        <Typography variant="body1">Aspiring Front End Developer</Typography>
-        <div style={{ marginBottom: ".5rem" }}>
-          <IconButton
-            onClick={() => window.open(githubUrl, "_blank")}
-            style={{ backgroundColor: "transparent" }}
-            onMouseEnter={() => handleHover("github")}
-            onMouseLeave={() => setIsHovered("")}
-          >
-            <GitHubIcon
-              fontSize="large"
-              style={{
-                color: isHovered === "github" ? "#ff79c6" : "#bd93f9",
-                backgroundColor: "transparent",
-              }}
-            />
-          </IconButton>
-          <IconButton
-            onClick={() => window.open(linkedInUrl, "_blank")}
-            style={{ backgroundColor: "transparent" }}
-            onMouseEnter={() => handleHover("linkedin")}
-            onMouseLeave={() => setIsHovered("")}
-          >
-            <LinkedInIcon
-              fontSize="large"
-              style={{
-                color: isHovered === "linkedin" ? "#ff79c6" : "#bd93f9",
-                backgroundColor: "transparent",
-              }}
-            />
-          </IconButton>
+        <Typography variant='body1'>Aspiring Front End Developer</Typography>
+        <div style={{ marginBottom: '.5rem' }}>
+          <SocialButton
+            socialNetwork='linkedin'
+            isHovered={isHovered}
+            handleHover={handleHover}
+            setIsHovered={setIsHovered}
+          />
+          <SocialButton
+            socialNetwork='github'
+            isHovered={isHovered}
+            handleHover={handleHover}
+            setIsHovered={setIsHovered}
+          />
         </div>
       </div>
     </div>
---
+++ src/components/SocialButton.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { IconButton } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+interface SocialButtonProps {
+  socialNetwork: 'linkedin' | 'github';
+  isHovered: string;
+  handleHover: (name: string) => void;
+  setIsHovered: (name: string) => void;
+}
+
+const SocialButton = ({
+  socialNetwork,
+  isHovered,
+  handleHover,
+  setIsHovered,
+}: SocialButtonProps) => {
+  const githubUrl: string = 'https://github.com/hberglund';
+  const linkedInUrl: string =
+    'https://linkedin.com/in/herman-berglund-8b8939100';
+
+  return (
+    <IconButton
+      onClick={() =>
+        window.open(
+          socialNetwork === 'linkedin' ? linkedInUrl : githubUrl,
+          '_blank'
+        )
+      }
+      style={{ backgroundColor: 'transparent' }}
+      onMouseEnter={() =>
+        socialNetwork === 'linkedin'
+          ? handleHover('linkedin')
+          : handleHover('github')
+      }
+      onMouseLeave={() => setIsHovered('')}
+    >
+      {socialNetwork === 'linkedin' ? (
+        <LinkedInIcon
+          fontSize='large'
+          style={{
+            color: isHovered === 'linkedin' ? '#ff79c6' : '#bd93f9',
+            backgroundColor: 'transparent',
+          }}
+        />
+      ) : (
+        <GitHubIcon
+          fontSize='large'
+          style={{
+            color: isHovered === 'github' ? '#ff79c6' : '#bd93f9',
+            backgroundColor: 'transparent',
+          }}
+        />
+      )}
+    </IconButton>
+  );
+};
+
+export default SocialButton;
